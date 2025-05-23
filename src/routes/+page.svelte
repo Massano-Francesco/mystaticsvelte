5e79d1c9b14ae337da366ebb1dd9a3e4938c7622
Massano-Francesco <massano.francesco@istitutoagnelli.it>
2025-05-23 12:17:54 +0200

    prova progetto svelte static

---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<ul>
+    <li><a href="/about">about</a></li>
+</ul>
