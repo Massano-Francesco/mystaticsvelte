758c5c5fcc294620c1b9799d88ad9eb04f43803e
Massano-Francesco <massano.francesco@istitutoagnelli.it>
2025-05-23 12:28:51 +0200

    aggiustamento pagina about

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,5 +2,5 @@
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
 <ul>
-    <li><a href="/about">about</a></li>
+    <li><a href="/mysveltestatic/about">about</a></li>
 </ul>
